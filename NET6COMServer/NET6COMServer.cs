8874eeed05afc53890a1ce37e0fa8272e5b3955e
komiyamma <komiyama0629@gmail.com>
2024-03-27 14:54:11 +0900

    こみっちょん

---
+++ NET6COMServer/NET6COMServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NET6COMServer
+{
+
+    [ComVisible(true)]
+    [Guid("2231CB75-2BD5-40A6-907F-75F2116AF699")]
+    public class NET6COMServer
+    {
+        public string str_add(string a, string b)
+        {
+            return a + b;
+        }
+
+        public int int_add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
