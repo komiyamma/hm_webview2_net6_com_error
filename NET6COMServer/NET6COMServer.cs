7cb3da9be861af86668ed1ae26ced8a77fecdce9
komiyamma <komiyama0629@gmail.com>
2024-03-27 15:10:46 +0900

    こみっちょん２

--- NET6COMServer/NET6COMServer.cs
+++ NET6COMServer/NET6COMServer.cs
@@ -3,11 +3,21 @@ using System.Runtime.InteropServices;
 
 namespace NET6COMServer
 {
-
     [ComVisible(true)]
     [Guid("2231CB75-2BD5-40A6-907F-75F2116AF699")]
     public class NET6COMServer
     {
+        public int this[int x, int y]
+        {
+            get
+            {
+                return 3;
+            }
+            set
+            {
+
+            }
+        }
         public string str_add(string a, string b)
         {
             return a + b;
@@ -16,6 +26,21 @@ namespace NET6COMServer
         public int int_add(int a, int b)
         {
             return a + b;
+        }
+
+        private int _aaa = 3;
+        public int aaa
+        {
+            get
+            {
+                return _aaa;
+            }
+
+            set
+            {
+                _aaa = value + 1;
+            }
+
         }
     }
 }
--- NET6COMServer/obj/Debug/net6.0/NET6COMServer.AssemblyInfo.cs
+++ NET6COMServer/obj/Debug/net6.0/NET6COMServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NET6COMServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8874eeed05afc53890a1ce37e0fa8272e5b3955e")]
 [assembly: System.Reflection.AssemblyProductAttribute("NET6COMServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NET6COMServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
